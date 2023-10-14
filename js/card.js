e76c4cf0c1c553db067f127eeeee43bc502e7551
poonsgollamudi <pgollamudi@gmail.com>
2023-10-13 21:53:17 -0400

    added cards with javascript

---
+++ js/card.js
@@ -0,0 +1,41 @@
+import jsonData from "../data/animes.json" assert { type: "json" };
+
+var divsCard = document.querySelectorAll(".empty");
+//console.log(divsCard);
+var divs_len = document.getElementsByClassName("empty").length;
+
+for (let i = 0; i < divs_len; i++) {
+  var card = `
+  <div class="card">
+    <img
+      src=${jsonData.animes[i].img}
+      class="card-img-top"
+      height="300px"
+      alt="Sunset Over the Sea"
+    />
+    <div class="card-body text-center">
+      <h5 class="card-title">${jsonData.animes[i].name}</h5>
+      <a
+        href=${jsonData.animes[i].desc_link}
+        target="_blank"
+        class="btn btn-primary"
+      >
+        Learn More
+      </a>
+      <a
+        href=${jsonData.animes[i].utube_link}
+        target="_blank"
+        class="btn btn-primary"
+      >
+        Favourite OST
+      </a>
+    </div>
+  </div>
+`;
+
+  var doc = document.createElement("div");
+  doc.id = "id_" + i;
+  doc.innerHTML = card;
+  //console.log(divsCard[i]);
+  divsCard[i].appendChild(doc);
+}
